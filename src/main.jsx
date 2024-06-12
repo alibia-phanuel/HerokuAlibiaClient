f8b27d72b953e170458ea88af6dbbfd292419d4e
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2024-06-12 20:28:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ReactGA from "react-ga4";
+  //YOUR_TRACKING_ID
+  ReactGA.initialize("YOUR_TRACKING_ID");
+  //YOUR_TRACKING_ID
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
