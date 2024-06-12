ea30e51d84fc7997a65f659cf02829c1c44bd25c
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2024-06-12 20:27:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import ReactGA from "react-ga4";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import ReactGA from "react-ga4";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
